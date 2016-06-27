b25de984be792668daca5f217e0984adc821dda1
Carlos Ríos V <crosvera@gmail.com>
2016-06-26 23:21:19 -0400

    paire review homework

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    M <- NULL
+    set <- function(y) {
+        x <<- y
+        M <<- NULL
+    }
+    get <- function()  x 
 
+    setinv <- function(inverse) M <<-  inverse 
+
+    getinv <- function() M
+
+    list(set = set, get = get, setinv = setinv,
+         getinv = getinv)
 }
 
 
@@ -12,4 +24,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    M <- x$getinv()
+
+    if(!is.null(M)) {
+        message("Getting data from cache")
+
+        return(M)
+    }
+    
+    data <- x$get()
+    M <- solve(data, ...)
+
+    x$setinv(M)
+    
+    M
 }
